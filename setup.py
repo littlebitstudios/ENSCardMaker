d7de428a81049bcac00b4fe0ed7c9663e69c706a
LittleBit <littlebit@littlebitstudios.com>
2024-07-28 19:08:59 -0500

    chore: Update version to 1.0.1 and add pydenticon dependency

--- setup.py
+++ setup.py
@@ -6,17 +6,27 @@ long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 setup(
     name='ENSCardMaker',
-    version='1.0',
+    version='1.0.1',
+    description='Takes an ENS name or ETH address and returns a card based on information from an ENS profile.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='LittleBit',
+    author_email='littlebit@littlebitstudios.com',
+    license='MIT',
+    license_files='LICENSE',
     packages=find_packages(),
     install_requires=[
         'requests',
         'Pillow',
+        'pydenticon'
     ],
     entry_points={
         'console_scripts': [
-            'enscardmaker=ENSCardMaker.__main__:main',
+            'enscardmaker=ENSCardMaker:main',
         ],
     },
-    long_description=long_description,
-    long_description_content_type='text/markdown',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
 )
